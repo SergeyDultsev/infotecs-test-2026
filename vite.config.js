2d706842f5df8c1dd79d1eebe62d8ce19c339de4
SergeyDultsev <dultsev010904@gmail.com>
2026-07-20 14:40:22 +0700

    fix: deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/infotecs-test/",
+  base: "/infotecs-test-2026/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
