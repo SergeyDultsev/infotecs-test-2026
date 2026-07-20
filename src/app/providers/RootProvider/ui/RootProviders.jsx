d6f39c3fad91299cc18d36d814c328d5513cf93c
SergeyDultsev <dultsev010904@gmail.com>
2026-07-20 14:42:06 +0700

    fix: deploy

--- src/app/providers/RootProvider/ui/RootProviders.jsx
+++ src/app/providers/RootProvider/ui/RootProviders.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { ModalProvider } from "@app/providers/ModalProvider/ui/ModalProvider.jsx";
 
 const RootProviders = ({ children }) => (
-    <BrowserRouter basename="/infotecs-test">
+    <BrowserRouter basename="/infotecs-test-2026">
         <ModalProvider>
             {children}
         </ModalProvider>
